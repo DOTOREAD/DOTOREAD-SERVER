3f72d39b0c36cb31703f2af0cc74c97feb770d5c
sangeun <olsang1203@gmail.com>
2024-09-13 16:31:16 +0900

    first commit

---
+++ src/main/java/likelion/dotoread/DotoreadApplication.java
@@ -0,0 +1,13 @@
+package likelion.dotoread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotoreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotoreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion/dotoread/DotoreadApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.dotoread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotoreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
