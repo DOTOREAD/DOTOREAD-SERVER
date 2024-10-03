78ab883dc8f70d2bd39869f159b3d9df08d2b6bc
sangeun <olsang1203@gmail.com>
2024-10-04 00:54:02 +0900

    fix : User 컬럼 수정

--- src/main/java/likelion/dotoread/domain/User.java
+++ src/main/java/likelion/dotoread/domain/User.java
@@ -20,6 +20,9 @@ public class User extends BaseEntity {
     private String name;
     private String nickname;
     private String email;
+    private String accessToken;
+    private String username;
+    private String role;
     @Builder.Default
     private Integer storageCount = 5;
     @Builder.Default
@@ -30,5 +33,19 @@ public class User extends BaseEntity {
     private List<AcornUse> acornUseList = new ArrayList<>();
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<UserMission> userMissionList = new ArrayList<>();
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNickname(String nickname) {
+
+        this.nickname = nickname;
+    }
 
 }
