523361f57b6e56859099fec8225b8fbdb02d6fd9
sangeun <olsang1203@gmail.com>
2024-10-10 21:53:13 +0900

    test

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-docker stop instagram-server || true
-docker rm instagram-server || true
+docker stop dotoread || true
+docker rm dotoread || true
 docker pull ${ECR_ADDRESS}:latest
 docker run -d --name dotoread -p 8080:8080 ${ECR_ADDRESS}:latest
 echo "--------------- 서버 배포 끝 -----------------"
