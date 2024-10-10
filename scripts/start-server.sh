e2f6a23d054ae9397fe3cbd28b325d062b975779
sangeun <olsang1203@gmail.com>
2024-10-11 00:56:56 +0900

    test

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/dotoread-server
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+docker stop dotoread || true
+docker rm dotoread || true
+docker pull 474668395121.dkr.ecr.ap-northeast-2.amazonaws.com/dotoread:latest
+docker run -d --name instagram-server -p 8080:8080 474668395121.dkr.ecr.ap-northeast-2.amazonaws.com/dotoread:latest
 echo "--------------- 서버 배포 끝 -----------------"
