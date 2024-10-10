e80fe0900c2dc043fc501c150e2d743aeb4a361b
sangeun <olsang1203@gmail.com>
2024-10-10 21:39:36 +0900

    도커 추가

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/dotoread-server
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+docker stop instagram-server || true
+docker rm instagram-server || true
+docker pull ${ECR_ADDRESS}:latest
+docker run -d --name dotoread -p 8080:8080 ${ECR_ADDRESS}:latest
 echo "--------------- 서버 배포 끝 -----------------"
