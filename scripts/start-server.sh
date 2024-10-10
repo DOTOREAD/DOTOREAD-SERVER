1245b4aef30e62e120468965e0271289e604f9bb
sangeun <olsang1203@gmail.com>
2024-10-11 01:34:52 +0900

    test

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop dotoread || true
 docker rm dotoread || true
-docker pull 474668395121.dkr.ecr.ap-northeast-2.amazonaws.com/dotoread:latest
-docker run -d --name dotoread -p 8080:8080 474668395121.dkr.ecr.ap-northeast-2.amazonaws.com/dotoread:latest
+docker pull ${ECR_ADDRESS}:latest
+docker run -d --name dotoread -p 8080:8080 ${ECR_ADDRESS}:latest
 echo "--------------- 서버 배포 끝 -----------------"
--- src/main/java/likelion/dotoread/web/controller/testController.java
+++ src/main/java/likelion/dotoread/web/controller/testController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @GetMapping("/test")
     public ApiResponse test() {
-        return ApiResponse.onSuccess("please~~~");
+        return ApiResponse.onSuccess("^_^");
     }
 }
