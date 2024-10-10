5c13ab7b03b56a9a0ee70ca1b54fbaba45dab122
sangeun <olsang1203@gmail.com>
2024-10-11 00:21:44 +0900

    test

--- src/main/java/likelion/dotoread/web/controller/testController.java
+++ src/main/java/likelion/dotoread/web/controller/testController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @GetMapping("/test")
     public ApiResponse test() {
-        return ApiResponse.onSuccess("성공입니다.");
+        return ApiResponse.onSuccess("수정 잘 됩니다.");
     }
 }
