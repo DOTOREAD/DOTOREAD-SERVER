dd77b22061bba23549d60557f00b826d64c358df
sangeun <olsang1203@gmail.com>
2024-10-14 19:53:10 +0900

    test

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
+
 echo "--------------- 서버 배포 시작 -----------------"
-
-# DOTOREAD-NLP 저장소 클론 또는 업데이트
-if [ -d "DOTOREAD-NLP" ]; then
-  echo "DOTOREAD-NLP 저장소 업데이트"
-  cd DOTOREAD-NLP
-  git pull
-  cd ..
-else
-  echo "DOTOREAD-NLP 저장소 클론"
-  git clone https://github.com/DOTOREAD/DOTOREAD-NLP.git
-fi
-
-# ECR 로그인
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 474668395121.dkr.ecr.ap-northeast-2.amazonaws.com
-
-# 기존 컨테이너 중지 및 제거
-docker-compose down
-
-# 최신 이미지 풀
+docker stop dotoread || true
+docker rm dotoread || true
 docker pull 474668395121.dkr.ecr.ap-northeast-2.amazonaws.com/dotoread:latest
-
-# 컨테이너 시작
-docker-compose up -d
-
+docker run -d --name dotoread -p 8080:8080 474668395121.dkr.ecr.ap-northeast-2.amazonaws.com/dotoread:latest
 echo "--------------- 서버 배포 끝 -----------------"
