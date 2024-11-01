e22f7358a4ac6fe5c661446be4159c8c0ae283ea
Yeonjae37 <jyj030307@gmail.com>
2024-11-01 22:03:36 +0900

    style: 날짜 형식 변경 (프론트 요청 사항)

--- src/main/java/likelion/dotoread/response/BookmarkDetailResponse.java
+++ src/main/java/likelion/dotoread/response/BookmarkDetailResponse.java
@@ -4,18 +4,20 @@ import likelion.dotoread.domain.Bookmark;
 import likelion.dotoread.domain.Folder;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import likelion.dotoread.dto.FolderDTO;
 
 public record BookmarkDetailResponse(Long bookmarkId, String title,
-                                     String url, String img, LocalDateTime createdAt, FolderDTO folder) {
+                                     String url, String img, String createdAt, FolderDTO folder) {
 
     public static BookmarkDetailResponse of(final Long bookmarkId, final String title, final String url,
                                             String img, final LocalDateTime createdAt, Folder folder) {
+        String formattedDate = createdAt != null ? createdAt.format(DateTimeFormatter.ofPattern("M월 d일")) : null;
         FolderDTO folderDTO = folder != null ? FolderDTO.from(folder) : null;
-        return new BookmarkDetailResponse(bookmarkId, title, url, img, createdAt, folderDTO);
+        return new BookmarkDetailResponse(bookmarkId, title, url, img, formattedDate, folderDTO);
     }
 
     public static List<BookmarkDetailResponse> from(final List<Bookmark> bookmarks) {
