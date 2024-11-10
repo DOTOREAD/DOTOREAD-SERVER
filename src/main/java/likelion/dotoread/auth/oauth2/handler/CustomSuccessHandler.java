3723ce4aa6d0c4e419bbe0b3aa625ae3f3c4c500
sangeun <olsang1203@gmail.com>
2024-11-10 12:10:12 +0900

    refactor : https 테스트

--- src/main/java/likelion/dotoread/auth/oauth2/handler/CustomSuccessHandler.java
+++ src/main/java/likelion/dotoread/auth/oauth2/handler/CustomSuccessHandler.java
@@ -88,8 +88,9 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 //        objectMapper.writeValue(response.getWriter(), apiResponse);
 
 //        response.sendRedirect("http://localhost:8080");
+        response.sendRedirect("https://api.dotoread.shop/health");
 //        response.sendRedirect("http://localhost:5173");
-        response.sendRedirect("http://localhost:5173?loggedIn=true");
+//        response.sendRedirect("http://localhost:5173?loggedIn=true");
 
     }
 
