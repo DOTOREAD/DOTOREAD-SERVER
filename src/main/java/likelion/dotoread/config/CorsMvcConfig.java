b1438d3442a721c69f36bd3e8fc7bf5010619f84
Yeonjae37 <jyj030307@gmail.com>
2024-11-13 22:35:42 +0900

    fix: cors 오류 해결1

--- src/main/java/likelion/dotoread/config/CorsMvcConfig.java
+++ src/main/java/likelion/dotoread/config/CorsMvcConfig.java
@@ -14,7 +14,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://localhost:8080",
-                        SPRING_SERVER_URL, "http://localhost:5173")
+                        SPRING_SERVER_URL, "http://localhost:5173", "https://api.dotoread.shop", "http://api.dotoread.shop")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Set-Cookie", "Authorization")
