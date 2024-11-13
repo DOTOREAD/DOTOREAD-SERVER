8798c274dbeaa698ebec6024d74db6c4b061566b
sangeun <olsang1203@gmail.com>
2024-11-14 04:02:13 +0900

    refactor : AcornAdd-UserMission 1대다로 변경

--- src/main/java/likelion/dotoread/domain/mapping/UserMission.java
+++ src/main/java/likelion/dotoread/domain/mapping/UserMission.java
@@ -7,7 +7,8 @@ import likelion.dotoread.domain.User;
 import likelion.dotoread.domain.common.BaseEntity;
 import lombok.*;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,15 +19,20 @@ public class UserMission extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private LocalDateTime completedAt;
-    @Builder.Default
-    private Integer current = 0; //현재진행횟수
+    private Integer current;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "mission_id")
     private Mission mission;
-    @OneToOne(mappedBy = "userMission", cascade = CascadeType.ALL)
-    private AcornAdd acornAdd;
+    @OneToMany(mappedBy = "userMission", cascade = CascadeType.ALL)
+    private List<AcornAdd> acornAdds = new ArrayList<>();
+
+    public void setCurrent(){
+        this.current++;
+    }
+    public void resetCurrent(){
+        this.current = 0;
+    }
 }
