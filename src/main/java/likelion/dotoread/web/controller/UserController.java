f7685101a14a65507a2a189d1ed100e51bafcab4
sangeun <olsang1203@gmail.com>
2024-11-15 01:08:35 +0900

    refactor : 테스트를 위해 토큰 유효 시간 증가

--- src/main/java/likelion/dotoread/web/controller/UserController.java
+++ src/main/java/likelion/dotoread/web/controller/UserController.java
@@ -44,7 +44,8 @@ public class UserController {
         String role = jwtUtil.getRole(refresh);
 
         //make new JWT
-        String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
+        //TODO : 토큰 시간 줄이기
+        String newAccess = jwtUtil.createJwt("access", username, role, 86400000L);
         String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
         //Refresh 토큰 저장 DB에 기존의 Refresh 토큰 삭제 후 새 Refresh 토큰 저장
         refreshRepository.deleteByRefresh(refresh);
