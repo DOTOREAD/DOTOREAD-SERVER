54856bc571429a1ecd425a913c1b53df833a8f9e
sangeun <olsang1203@gmail.com>
2024-11-21 23:44:30 +0900

    feat : repository 생성

---
+++ src/main/java/likelion/dotoread/repository/CollectionLikeRepository.java
@@ -0,0 +1,7 @@
+package likelion.dotoread.repository;
+
+import likelion.dotoread.domain.mapping.CollectionLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CollectionLikeRepository extends JpaRepository<CollectionLike, Long> {
+}
