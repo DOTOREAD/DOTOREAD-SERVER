c7cb7416e630cc9d2d16666405734ff2feeeb1e4
sangeun <olsang1203@gmail.com>
2024-11-22 03:21:35 +0900

    fix : 유저 본인의 미션만 조회하도록 수정

--- src/main/java/likelion/dotoread/repository/UserMissionRepository.java
+++ src/main/java/likelion/dotoread/repository/UserMissionRepository.java
@@ -3,8 +3,15 @@ package likelion.dotoread.repository;
 import likelion.dotoread.domain.User;
 import likelion.dotoread.domain.mapping.UserMission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserMissionRepository extends JpaRepository<UserMission, Long> {
     UserMission findByUserAndMissionId(User user, Long missionId);
     boolean existsByUser(User user);
+
+    @Query("SELECT m FROM UserMission m WHERE m.user = :user")
+    List<UserMission> findAllByUser(@Param("user") User user);
 }
